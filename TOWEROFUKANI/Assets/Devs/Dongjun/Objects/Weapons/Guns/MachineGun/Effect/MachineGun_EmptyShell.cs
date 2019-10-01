f26691f15de02fe0628805801f2d9861e1da8005
doongjohn <doongjohn@gmail.com>
2019-10-02 02:14:11 +0900

    머신건 탄피 버그 수정

--- TOWEROFUKANI/Assets/Devs/Dongjun/Objects/Weapons/Guns/MachineGun/Effect/MachineGun_EmptyShell.cs
+++ TOWEROFUKANI/Assets/Devs/Dongjun/Objects/Weapons/Guns/MachineGun/Effect/MachineGun_EmptyShell.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MachineGun_EmptyShell : PlayerDropObj
 {
@@ -12,6 +10,6 @@ public class MachineGun_EmptyShell : PlayerDropObj
         base.ResetOnActive();
 
         rb2D.AddForce((Vector3.up - transform.right).normalized * Random.Range(forceMin, forceMax), ForceMode2D.Impulse);
-        rb2D.AddTorque(Random.Range(forceMin, forceMax), ForceMode2D.Impulse);
+        rb2D.AddTorque(Mathf.Sign(transform.right.x) * Random.Range(forceMin, forceMax), ForceMode2D.Impulse);
     }
 }
