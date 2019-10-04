be6350985cb52e4b50dc1a1f95c18e6ebd35ee8b
doongjohn <doongjohn@gmail.com>
2019-10-05 03:14:53 +0900

    플레이어 코드 정리

--- TOWEROFUKANI/Assets/Devs/Dongjun/Objects/Player/Player.cs
+++ TOWEROFUKANI/Assets/Devs/Dongjun/Objects/Player/Player.cs
@@ -2,9 +2,12 @@
 
 public class Player : CLA_Main
 {
+    #region Var: CLA_Action
     private Player_Movement_Action movement_AC;
     private Player_Dash_Action dash_AC;
+    #endregion
 
+    #region Method: Init
     protected override void Init()
     {
         movement_AC = GetComponent<Player_Movement_Action>();
@@ -13,7 +16,9 @@ public class Player : CLA_Main
         ConditionLogics.Add(movement_AC, CL_Movement);
         ConditionLogics.Add(dash_AC, CL_Dash);
     }
+    #endregion
 
+    #region Method: Condition Logic
     private CLA_Action CL_Movement()
     {
         if (PlayerInputManager.Inst.Input_DashDir != 0)
@@ -23,12 +28,15 @@ public class Player : CLA_Main
     }
     private CLA_Action CL_Dash()
     {
+        // Cancle On Jump
         if (movement_AC.JumpData.canJump && PlayerInputManager.Inst.Input_Jump)
             return movement_AC;
 
+        // Cancle On End
         if (!dash_AC.IsDasing)
             return movement_AC;
 
         return dash_AC;
     }
+    #endregion
 }
