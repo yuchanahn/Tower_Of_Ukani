cec0227941a45333f5b4d2920a356b851e0a6ce9
= <=>
2019-11-21 03:07:48 +0900

    성능향상JPS스크립트 생성

--- TOWEROFUKANI/Assets/Devs/Yuchan/Mob/Core/CliffDetectionData.cs
+++ TOWEROFUKANI/Assets/Devs/Yuchan/Mob/Core/CliffDetectionData.cs
@@ -16,8 +16,8 @@ public class CliffDetect_Logic
         int uns = sizeX == 0 ? 0 : sizeX > 0 ? 1 : -1;
         var hit = Physics2D.RaycastAll(Tr.position, new Vector2(uns, -1), Mathf.Abs(sizeX), grdLayer);
         var hit2 = Physics2D.RaycastAll(Tr.position + new Vector3(sizeX * 0.5f, 0), new Vector2(0, -1), fallHeight, grdLayer);
-        Debug.DrawRay(Tr.position, new Vector2(uns, -1) * Mathf.Abs(sizeX), Color.red);
-        Debug.DrawRay(Tr.position + new Vector3(sizeX * 0.5f, 0), new Vector2(0, -1) * fallHeight, Color.red);
+        //Debug.DrawRay(Tr.position, new Vector2(uns, -1) * Mathf.Abs(sizeX), Color.red);
+        //Debug.DrawRay(Tr.position + new Vector3(sizeX * 0.5f, 0), new Vector2(0, -1) * fallHeight, Color.red);
         return (hit.Length > 0) || (hit2.Length > 0);
     }
 
@@ -30,9 +30,9 @@ public class CliffDetect_Logic
 
         var sx = TPos - new Vector2(size.x / 2, 0);
 
-        for (float i = 0.005f; sx.x < (TPos + new Vector2(size.x / 2, 0)).x; sx.x += i)
+        //for (float i = 0.005f; sx.x < (TPos + new Vector2(size.x / 2, 0)).x; sx.x += i)
         {
-            Debug.DrawRay(sx, (Vector2.down * fallHeight), Color.green);
+            //Debug.DrawRay(sx, (Vector2.down * fallHeight), Color.green);
         }
         return Physics2D.BoxCastAll(TPos, size, 0, Vector2.down, fallHeight, grdLayer).Length > 0;
     }
@@ -42,18 +42,15 @@ public class CliffDetect_Logic
         var sx = Pos - new Vector2(size.x / 2, 0);
         sx.y += size.y / 2;
         size.y *= 0.99f;
-        for (float i = 0.005f; sx.x < (Pos + new Vector2(size.x / 2, 0)).x + (Dir * Speed * Time.fixedDeltaTime); sx.x += i)
+        //for (float i = 0.005f; sx.x < (Pos + new Vector2(size.x / 2, 0)).x + (Dir * Speed * Time.fixedDeltaTime); sx.x += i)
         {
-            Debug.DrawRay(sx, (Vector2.down * size.y), Color.red);
+            //Debug.DrawRay(sx, (Vector2.down * size.y), Color.red);
         }
 
         var obj = Physics2D.BoxCastAll(Pos, size, 0, Vector2.right * Dir, (Speed * Time.fixedDeltaTime), wallLayer);
 
-        foreach(var i in obj)
-        {
-            Debug.Log(i.collider.gameObject.name);
-        }
+ 
 
-        return !(Physics2D.BoxCastAll(Pos, size, 0, Vector2.right * Dir, (Speed * Time.fixedDeltaTime), wallLayer).Length > 0);
+        return !(obj.Length > 0);
     }
 }
