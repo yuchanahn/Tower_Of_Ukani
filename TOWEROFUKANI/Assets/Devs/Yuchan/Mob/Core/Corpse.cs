bad99de4f8c10a0ec31dc09f29a8f54381d83868
안유찬 노트북 <anyc123@hanmail.net>
2020-01-14 02:11:36 +0900

    시체 조각 흡수 구현.

--- TOWEROFUKANI/Assets/Devs/Yuchan/Mob/Core/Corpse.cs
+++ TOWEROFUKANI/Assets/Devs/Yuchan/Mob/Core/Corpse.cs
@@ -22,10 +22,57 @@ public class Corpse : PoolingObj
         ATimer.Pop(GetInstanceID().ToString());
     }
 
+    Rigidbody2D rb2D = null;
+    Collider2D col;
+
+    float GravityScale = 0;
+    private void Start()
+    {
+
+    }
+
     public override void ResetOnSpawn()
     {
-        var rb2D = GetComponent<Rigidbody2D>();
+        if (rb2D == null)
+        {
+            rb2D = GetComponent<Rigidbody2D>();
+            col = GetComponent<Collider2D>();
+            GravityScale = rb2D.gravityScale;
+        }
+
+        col.enabled = true;
         rb2D.isKinematic = false;
         rb2D.velocity = new Vector2(0, 0);
+        acc = 0;
+        rb2D.gravityScale = GravityScale;
+        transform.localScale = Vector3.one;
+        StartAbsorb();
+    }
+
+    bool mAbsorb = false;
+    public void StartAbsorb()
+    {
+        mAbsorb = true;
+        GetComponent<Collider2D>().enabled = false;
+        rb2D.gravityScale = 0;
+    }
+
+
+    float acc = 1;
+    float aT = 0.1f;
+    float speed = 2;
+    private void Update()
+    {
+        if (!mAbsorb) return;
+        // 시체조각 흡수시 작아지는 연출....
+        var vel = (GM.PlayerPos - transform.position).normalized;
+        transform.position += vel * Time.deltaTime * speed;
+        transform.localScale = transform.localScale.x <= 0 ? Vector3.zero : transform.localScale - (Vector3.one * acc * Time.deltaTime);
+        
+        if (Vector2.Distance(GM.PlayerPos, transform.position) <= 0.1f)
+        {
+            this.Sleep();
+        }
+        acc += aT * Time.deltaTime;
     }
 }
