b01938cfd2e536894ce821e930b3cf997347b257
doongjohn <doongjohn@gmail.com>
2020-02-10 21:39:09 +0900

    뭔지 몰라

---
+++ TOWEROFUKANI/Assets/Devs/Dongjun/Item/_Base/Item/Weapon/FistBase.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public abstract class FistController<T> : WeaponController<T> where T : FistItem { }
+public abstract class FistItem : WeaponItem
+{
+    #region Var: Animation Names
+    public readonly string ANIM_Neutral = "Idle";
+    #endregion
+
+    #region Var: Stats
+    // Timer
+    public readonly TimerStat basicAttackTimer = new TimerStat();
+    #endregion
+
+    #region Method: Unity
+    protected virtual void Start()
+    {
+        // Init Timer
+        basicAttackTimer.SetTick(gameObject).ToEnd();
+    }
+    #endregion
+
+    #region Method: Stats
+    public override void ResetStats()
+    {
+        attackData.Reset();
+        basicAttackTimer.EndTime.Reset();
+    }
+    #endregion
+}
