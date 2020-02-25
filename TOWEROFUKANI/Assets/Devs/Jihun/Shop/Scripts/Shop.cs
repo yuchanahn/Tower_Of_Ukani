2ad90bdcb193a302191b6f2417e426071857cd3d
897754 <42639194+897754@users.noreply.github.com>
2020-02-25 18:17:34 +0900

    상점 만드는중

---
+++ TOWEROFUKANI/Assets/Devs/Jihun/Shop/Scripts/Shop.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public Transform slotRoot;
+
+    [SerializeField] private ShopSlot slotPrefab;
+
+    private List<ShopSlot> slots = new List<ShopSlot>();
+
+    private void Start()
+    {
+        Init();
+    }
+
+    // 슬롯들을 리스트에 넣어줌.
+    void Init()
+    {
+        SellNewItem("Potion of Healing");
+        SellNewItem("Machinegun");
+        SellNewItem("Talisman of Protection");
+        //SellNewItem("Rose of Love");
+
+
+        int slotCnt = slotRoot.childCount;
+
+        for(int i = 0; i < slotCnt; i++)
+        {
+            ShopSlot slot = slotRoot.GetChild(i).GetComponent<ShopSlot>();
+
+            slots.Add(slot);
+        }
+    }
+
+    public void CloseShop()
+    {
+        gameObject.SetActive(false);
+    }
+    public void OpenShop()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // 판매 아이템 목록에 아이템 추가.
+    public void SellNewItem(string name, params object[] itemCost)
+    {
+        ShopSlot slot = Instantiate<ShopSlot>(slotPrefab);
+
+        slot.transform.SetParent(slotRoot);
+        slot.transform.localScale = Vector3.one;
+
+        slot.SetItem(ItemDB.Inst.Items[name].Info);
+    }
+}
---
+++ TOWEROFUKANI/Assets/Devs/Jihun/Shop/Scripts/ShopSlot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemInfo item;
+
+    public Image itemSpriteImage;
+    public Text itemNameTxt;
+    public Text itemDescTxt;
+
+    
+
+    public void SetItem(ItemInfo item)
+    {
+        this.item = item;
+
+        if (item == null) return;
+
+        itemSpriteImage.gameObject.SetActive(true);
+        itemSpriteImage.sprite = item.Icon;
+
+        itemNameTxt.text = item.ItemName;
+        itemDescTxt.text = item.ItemDesc;
+    }
+
+    public void BuyItem()
+    {
+        int curGold = 0;
+
+        //가격 체크
+        Item[] gold = PlayerInventoryManager.inventory.GetItems("Gold");
+
+        if (gold != null)
+        {
+            for (int i = 0; i < gold.Length; i++)
+            {
+                curGold += gold[i].Info.Count;
+            }
+
+            Debug.Log(curGold + "원");
+        }
+
+        ItemDB.Inst.SpawnDroppedItem(item.ItemName);
+    }
+}
