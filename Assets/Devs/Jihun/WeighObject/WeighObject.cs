f41e0e50712e32abb84affcd5f29f58bdaef11c0
897754 <42639194+897754@users.noreply.github.com>
2020-02-28 02:05:42 +0900

    엘리베이터 만듦

---
+++ Assets/Devs/Jihun/WeighObject/WeighObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class WeighObject : MonoBehaviour
+{
+    [SerializeField]LayerMask checkLayer;
+
+    float width = 0;
+    float height = 0;
+
+    float mass;
+    float gravityScale;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    // 변수 초기화
+    void Init()
+    {
+        Rigidbody2D _rg = GetComponent<Rigidbody2D>();
+        BoxCollider2D _bc = GetComponent<BoxCollider2D>();
+        mass = _rg.mass;
+        gravityScale = _rg.gravityScale;
+        width = _bc.size.x * transform.lossyScale.x;
+        height = _bc.size.y * transform.lossyScale.y;
+    }
+
+
+    public float GetDownForce(List<WeighObject> checkedArr = null)
+    {
+        float downForce = 0;
+
+        if(checkedArr == null)
+        {
+            checkedArr = new List<WeighObject>();
+        }
+
+        checkedArr.Add(this);
+
+        RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position, new Vector2(width, height), 0, Vector2.up, 1f, checkLayer);
+
+
+            var check =
+            from hit in hits
+            where (hit.transform.position.Foot(new Vector2(0, height)).y > transform.position.y)
+            where (!checkedArr.Exists(a => a.gameObject == hit.transform.gameObject))
+            where (hit.transform.GetComponent<WeighObject>())
+            //where (Mathf.Abs(hit.transform.GetComponent<Rigidbody2D>().velocity.y) < 0.1f)
+            select hit;
+
+        foreach(RaycastHit2D hit in check)
+        {
+            if (hit.transform.GetComponent<OBB_Player>() == null && hit.transform.GetComponent<Mob_Base>() == null)
+                downForce += hit.transform.GetComponent<WeighObject>().GetDownForce(checkedArr);
+            else
+            {
+                downForce += 100;
+                checkedArr.Add(hit.transform.GetComponent<WeighObject>());
+            }
+        }
+
+        downForce += (gravityScale * mass);
+
+        return downForce;
+    }
+}
---
+++ Assets/Devs/Jihun/WeighObject/WeighPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeighPlatform : WeighObject
+{
+    Rigidbody2D _rg;
+
+    [SerializeField]
+    float upForce = 0;
+    [SerializeField]
+    float speed = 0;
+    [SerializeField]
+    float speedRate = 1;
+
+    [SerializeField]
+    float maxVel = 5f;
+    [SerializeField]
+    float minVel = -5f;
+
+
+    private void Start()
+    {
+        _rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoUp();
+    }
+
+    void GoUp()
+    {
+        float force = (upForce - GetDownForce());
+
+        float result = force / speedRate * speed;
+        if (result > maxVel) result = maxVel;
+        if (result < minVel) result = minVel;
+
+        _rg.velocity = new Vector2(_rg.velocity.x, result);
+    }
+}
