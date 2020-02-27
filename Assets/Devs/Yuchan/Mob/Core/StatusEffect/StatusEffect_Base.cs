eda737d87dca2e3e1dd91748410df05f3a56532f
안유찬 노트북 <anyc123@hanmail.net>
2020-02-28 03:32:07 +0900

    ㅇ

--- Assets/Devs/Yuchan/Mob/Core/StatusEffect/StatusEffect_Base.cs
+++ Assets/Devs/Yuchan/Mob/Core/StatusEffect/StatusEffect_Base.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class StatusEffect_Base : MonoBehaviour
 {
     [SerializeField] protected int priority;
+    protected StatusEffect_Object SE_Obj;
 
     protected virtual void Start()
     {
+        SE_Obj = GetComponent<StatusEffect_Object>();
     }
 
     public virtual void SetValue<T>(SE_Stat<T> value, T data)
     {
         value.SetValue(priority, data);
     }
+
+    public virtual void SetValue<T>(SE_Stat<T> value, T data, int priority)
+    {
+        value.SetValue(priority, data);
+    }
+
 
     protected virtual void OnSEDestroy()
     {
---
+++ Assets/Devs/Yuchan/Mob/Core/StatusEffect/StatusEffect_Knokback.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusEffect_Knokback : StatusEffect_Base
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public static void Create(GameObject obj)
+    {
+        var SE_Obj = obj.GetComponent<StatusEffect_Object>();
+        if (SE_Obj is null) return;
+
+        StatusEffect_Knokback knokback = obj.GetComponent<StatusEffect_Knokback>();
+        // 각 상태이상에 대한 초기화 설정을 해준다.
+        if (knokback is null)
+            knokback = obj.AddComponent<StatusEffect_Knokback>();
+        
+        // 모든 옵션에 관한, 디폴트 우선 순위 설정, 높을수록 우선순위가 높음.
+        knokback.priority = 9;
+    }
+    void Update()
+    {
+        //넉백을 수행 시킬 코드블럭.
+        
+        SetValue(SE_Obj.StatusEffectSpeedMult, 0);
+        SetValue(SE_Obj.StatusEffectAttackAble, false);
+        SetValue(SE_Obj.StatusEffectFollowAble, false);
+        SetValue(SE_Obj.StatusEffectNoTask, true);
+    }
+
+    protected override void OnSEDestroy()
+    {
+        SetValue(SE_Obj.StatusEffectSpeedMult, 1f);
+        SetValue(SE_Obj.StatusEffectAttackAble, true);
+        SetValue(SE_Obj.StatusEffectFollowAble, true);
+        SetValue(SE_Obj.StatusEffectNoTask, false);
+    }
+}
